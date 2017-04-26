1c0ea892e2ebfc8637d99327f18d60ec998d75c4
Hari Balamani <harib@thoughtworks.com>
2017-04-26 20:21:03 +1000

    Adding home screen which can help us navigate to various screens

--- app/app.js
+++ app/app.js
@@ -6,9 +6,23 @@
 
 import React, {Component} from 'react';
 import {
-    AppRegistry,
+	AppRegistry,
 } from 'react-native';
 
-import Home from './screen/Home';
+import {StackNavigator} from 'react-navigation';
 
-AppRegistry.registerComponent('Finanz', () => Home);
+import Home from './screen/Home';
+import Cat from './screen/Cat';
+import Puppy from './screen/Puppy';
+import CatAndPuppy from './screen/CatAndPuppy';
+
+const app = StackNavigator({
+	Home: {screen: Home},
+	Puppy: {screen: Puppy},
+	Cat: {screen: Cat},
+	CatAndPuppy: {screen: CatAndPuppy},
+});
+
+export default app;
+
+AppRegistry.registerComponent('Finanz', () => app);
---
+++ app/screen/Cat.js
@@ -0,0 +1,96 @@
+/**
+ * Created by slash on 26/4/17.
+ */
+
+import React, {Component} from 'react';
+import {
+	AppRegistry,
+	StyleSheet,
+	Image,
+	View,
+	Text,
+	TouchableOpacity
+} from 'react-native';
+
+export default class Cat extends Component {
+
+	constructor({navigation}) {
+		super()
+		this.state = {
+			likes: 0,
+			dislikes: 0
+		}
+
+		console.log("inside constructor")
+	}
+
+	componentWillMount() {
+		console.log("inside componentWillMount")
+	}
+
+	componentDidMount() {
+		console.log("inside componentDidMount")
+	}
+
+	componentWillUpdate() {
+		console.log("inside componentWillUpdate")
+	}
+
+	componentDidUpdate() {
+		console.log("inside componentDidUpdate")
+	}
+
+	componentWillUnmount() {
+		console.log("inside componentWillUnmount")
+	}
+
+	increaseLikeCount() {
+		this.setState({likes: this.state.likes + 1});
+		console.log("liked by : ", this.state.likes);
+	}
+
+	increaseDislikeCount() {
+		this.setState({dislikes: this.state.dislikes + 1});
+		console.log("disliked by : ", this.state.dislikes);
+	}
+
+	render() {
+		console.log("inside render")
+		return (
+			<View style={styles.container}>
+				<Image source={require('../images/cat.jpg')} style={styles.image}/>
+
+				<View style={{width: 300, flexDirection: 'row', justifyContent: 'space-between'}}>
+
+					<TouchableOpacity onPress={() => this.increaseLikeCount()} style={{padding: 10}}>
+						<Image source={require('../images/like-icon.png')}/>
+						<Text style={{padding: 11}}>{this.state.likes} </Text>
+					</TouchableOpacity>
+
+					<TouchableOpacity onPress={() => this.increaseDislikeCount()} style={{padding: 10}}>
+						<Image source={require('../images/dislike-icon.png')}/>
+						<Text style={{padding: 11}}>{this.state.dislikes} </Text>
+					</TouchableOpacity>
+
+				</View>
+			</View>
+		);
+	}
+}
+
+Cat.navigationOptions = {title : "Cat"};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	image: {
+		height: 300,
+		width: 300,
+		borderColor: '#000000',
+		borderWidth: 2
+	}
+});
